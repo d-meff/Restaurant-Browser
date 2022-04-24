c9c9362fa146b77c6fb3d1fe30a385ad9fecf437
Danny <dannythekid1292@gmail.com>
2022-04-24 15:34:01 -0500

    added taco bell and pizza hut as options

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import arbys, kfc
+import arbys, kfc, pizzahut, tacobell
 from arbys import get_arbys
 from kfc import get_kfc
+from pizzahut import get_pizzahut
+from tacobell import get_tacobell
 
-print("Search for these Restauraunt Selections in your town: \n\n - Arby's\n - KFC\n\nPlease type the full restaurant name as a selection\n\nYou can also type 'q' to quit\n")
+print("Search for these Restauraunt Selections in your town: \n\n - Arby's\n - KFC\n - Pizza Hut \n - Taco Bell \n\nPlease type the full restaurant name as a selection\n\nYou can also type 'q' to quit\n")
 restaurant_selection = input("Which restauraunt would you like to search for?: ").lower()
 while restaurant_selection != 'q':
     if restaurant_selection == "Arby's".lower():
@@ -12,6 +14,16 @@ while restaurant_selection != 'q':
         break
     elif restaurant_selection == 'KFC'.lower():
         get_kfc()
+        print('')
+        print("Please restart the program to search again.")
+        break
+    elif restaurant_selection == "Pizza Hut".lower():
+        get_pizzahut()
+        print('')
+        print("Please restart the program to search again.")
+        break
+    elif restaurant_selection == "Taco Bell".lower():
+        get_tacobell()
         print('')
         print("Please restart the program to search again.")
         break
---
+++ pizzahut.py
@@ -0,0 +1,37 @@
+# IF YOUR TOWN DOES NOT HAVE A PIZZA HUT
+# -- It will redirect to the normal location search page (all states)
+
+# external modules
+import geocoder
+import webbrowser
+
+from geopy.geocoders import *
+from geocoder import location, ipinfo
+
+# own modules
+import states
+from states import states_abr
+
+def get_pizzahut():
+
+    your_location = (geocoder.ip("me"))
+    your_lat_long = your_location.latlng
+    your_state = str(your_location).split()[5][0:-1]
+    your_state_abbr = ""
+    your_town = str(your_location).split()[4][1:-1]
+    condensed_town = ""
+
+    condensed_town = your_town.replace(' ', '-')
+
+    for i in states_abr:
+        if states_abr.get(i) == your_state:
+            your_state_abbr = i.lower()
+            break
+
+    pizzahut_in_town = f'https://locations.pizzahut.com/{your_state_abbr.lower()}/{condensed_town.lower()}'
+
+    chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s" 
+
+    # If you have Google Chrome, replace 'chrome_path' with your file path. (Make sure to include the %s at the end.)
+
+    webbrowser.get(chrome_path).open_new_tab(pizzahut_in_town)
